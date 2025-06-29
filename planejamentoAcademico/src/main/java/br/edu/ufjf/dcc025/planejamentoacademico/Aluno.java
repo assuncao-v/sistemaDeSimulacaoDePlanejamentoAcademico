cd76771050345bd5b21a7647b946d5faaef159b1
Vitor Assuncao <vitor.assuncaosantos100@gmail.com>
2025-06-29 20:16:50 -0300

    29.06.2025-20:16-assuncao-v Criação da classe principal Aluno com os atributos nome, matricula, disciplinasCursadas e quantidadeHorasMaximas e os métodos adicionarDisciplinaCursada(Disciplina disciplina), getQuantidadeHorasMaximas(), setQuantidadeHorasMaximas(Integer quantidadeHorasMaximas), Aluno(String nome, String matricula, Integer quantidadeHorasMaxima), validarNome(String nome), validarMatricula(String matricula), validarQuantidadeHorasMaxima(Integer validadorQuantidadeHorasMaxima). O métodos de validação ainda não foram implementados com tratamento de exceção.

---
+++ planejamentoAcademico/src/main/java/br/edu/ufjf/dcc025/planejamentoacademico/Aluno.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ufjf.dcc025.planejamentoacademico;
+
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author assuncao-v
+ */
+public class Aluno{
+    public final String nome;
+    public final String matricula;
+    public final List<Disciplina> disciplinasCursadas;
+    private Integer quantidadeHorasMaxima;
+    //private Disciplina planejamentoFuturo; Não sei ainda como implementar. 
+    
+    //Precisa implementar validação dos parâmetros com tratamento de exceção
+    public Aluno(String nome, String matricula, Integer quantidadeHorasMaxima){
+        if(validarNome(nome)) this.nome = nome;
+        else this.nome = "";
+        if(validarMatricula(matricula)) this.matricula = matricula;
+        else this.matricula = "";
+        
+        disciplinasCursadas = new ArrayList<>();
+        
+        if(validarQuantidadeHorasMaxima(quantidadeHorasMaxima)) this.quantidadeHorasMaxima = quantidadeHorasMaxima;
+    }
+    
+    private boolean validarNome(String nome){
+        return true;
+    }
+    private boolean validarMatricula(String matricula){
+        return true;
+    }
+    private boolean validarQuantidadeHorasMaxima(Integer validadorQuantidadeHorasMaxima){
+        return true;
+    }
+    
+    public void adicionarDisciplinaCursada(Disciplina disciplina){
+        disciplinasCursadas.add(disciplina);
+    }
+    
+    public Integer getQuantidadeHorasMaxima(){
+        return this.quantidadeHorasMaxima;
+    }
+    
+    public void setQuantidadeHorasMaxima(Integer quantidadeHorasMaxima){
+        this.quantidadeHorasMaxima = quantidadeHorasMaxima;
+    }
+}
