6085c75f6ba2fe66ccce3caeda72966b069dd421
Vitor Assuncao <vitor.assuncaosantos100@gmail.com>
2025-06-29 20:22:39 -0300

    29.06.2025-20:22-assuncao-v Criação da classe principal Turma com os atributos codigoDaDisciplina, capacidadeMaxima e quantidadeAlunosMatriculados e os métodos Turma(String codigoDaDisciplina, Integer capacidadeMaxima), validarCodigoDaDisciplina(String codigoDaDisciplina), validarCapacidadeMaxima(Integer capacidadeMaxima) e getQuantidadeAlunosMatriculados(). Os métodos de validação não foram implementados com tratamento de exceções.

---
+++ planejamentoAcademico/src/main/java/br/edu/ufjf/dcc025/planejamentoacademico/Turma.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ufjf.dcc025.planejamentoacademico;
+
+/**
+ *
+ * @author assuncao-v
+ */
+public class Turma {
+    public final String codigoDaDisciplina;
+    public final Integer capacidadeMaxima;
+    //public final Horario horarioFixo; Devemos criar uma classe para Horario
+    private Integer quantidadeAlunosMatriculados;
+    
+    //Precisa de validação dos parâmetros com tratamento de exceção.
+    public Turma(String codigoDaDisciplina, Integer capacidadeMaxima /* ,Horario horarioFixo*/){
+        if(validarCodigoDaDisciplina(codigoDaDisciplina)) this.codigoDaDisciplina = codigoDaDisciplina;
+        else this.codigoDaDisciplina = codigoDaDisciplina;
+        if(validarCapacidadeMaxima(capacidadeMaxima)) this.capacidadeMaxima = capacidadeMaxima;
+        else this.capacidadeMaxima = 0;
+        //this.horarioFixo = horarioFixo;
+    }
+    
+    private boolean validarCodigoDaDisciplina(String codigoDaDisciplina){
+        return true;
+    }
+    private boolean validarCapacidadeMaxima(Integer capacidadeMaxima){
+        return true;
+    }
+    
+    public Integer getQuantidadeAlunosMatriculados(){
+        return this.quantidadeAlunosMatriculados;
+    }
+    
+}
