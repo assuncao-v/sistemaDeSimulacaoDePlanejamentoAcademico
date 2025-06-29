09fbe469076d5c81b641adb35c9da93eac5b0eca
Vitor Assuncao <vitor.assuncaosantos100@gmail.com>
2025-06-29 19:31:40 -0300

    29.06.2025-19:30-assuncao-v Criação da aplicação Java21 com Maven intulado planejamentoAcademico.

---
+++ planejamentoAcademico/src/main/java/br/edu/ufjf/dcc025/planejamentoacademico/PlanejamentoAcademico.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.edu.ufjf.dcc025.planejamentoacademico;
+
+/**
+ *
+ * @author assuncao-v
+ */
+public class PlanejamentoAcademico {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
