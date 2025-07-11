c2d005cba992a751c8441ee6f28409ab00d4fcc0
Welder Victor <weldersalvadorvictor12@hotmail.com>
2025-07-11 17:48:30 -0300

    Criação da classe repositorioTurmasDisciplinas
    A classe carrega os dados das disciplinas e turmas dos respectivos semestres listados

--- planejamentoAcademico/src/main/java/br/edu/ufjf/dcc025/planejamentoacademico/repositorioTurmasDisciplinas.java
+++ planejamentoAcademico/src/main/java/br/edu/ufjf/dcc025/planejamentoacademico/repositorioTurmasDisciplinas.java
@@ -8,11 +8,22 @@ public class repositorioTurmasDisciplinas {
         List<Disciplina> disciplinasObrigatorias = new ArrayList<>();
         Disciplina calc1 = new DisciplinaObrigatoria("MAT154","Calculo 1",4);
         Disciplina alg = new DisciplinaObrigatoria("DCC199","Algoritmos",4);
-        Disciplina algPratica = new DisciplinaObrigatoria("DCC199X","Algoritmos pratica",2);
+        Disciplina algPratica = new DisciplinaObrigatoria("DCC5199","Algoritmos pratica",2);
         Disciplina quimFundamental = new DisciplinaObrigatoria("QUI165","Quimica fundamental",4);
         Disciplina ga = new DisciplinaObrigatoria("MAT155","Geometria analitica e sistemas lineres",4);
         Disciplina labIntFis = new DisciplinaObrigatoria("FIS122","Laboratorio de introducao as ciencias fisicas",2);
         Disciplina intCiencias = new DisciplinaObrigatoria("ICE001","Introducao a ciencias exatas",2);
+        Disciplina labQuim = new DisciplinaObrigatoria("QUI126","Laboratorio de quimica",2);
+        Disciplina calc2 = new DisciplinaObrigatoria("MAT156","Calculo 2",4);
+        Disciplina alg2 = new DisciplinaObrigatoria("DCCXX1","Algoritmos 2",4);
+        Disciplina alg2Pratica = new DisciplinaObrigatoria("DCC5X1","Algoritmos pratica",2);
+        Disciplina fis1 = new DisciplinaObrigatoria("FIS073","Fisica 1",4);
+        Disciplina intEst = new DisciplinaObrigatoria("EST028","Introducao a estatistica",4);
+        Disciplina labFis1 = new DisciplinaObrigatoria("FIS073","Laboratorio de fisica 1",2);
+        Disciplina labTrans = new DisciplinaObrigatoria("QUI168","Laboratorio de transformacoes quimicas",2);
+        Disciplina intEng = new DisciplinaObrigatoria("MAC011","Introducao a engenharia computacional",2);
+
+        //1 Semestre
         disciplinasObrigatorias.add(calc1);
         disciplinasObrigatorias.add(alg);
         disciplinasObrigatorias.add(algPratica);
@@ -20,6 +31,15 @@ public class repositorioTurmasDisciplinas {
         disciplinasObrigatorias.add(ga);
         disciplinasObrigatorias.add(labIntFis);
         disciplinasObrigatorias.add(intCiencias);
+        //2 Semestre
+        disciplinasObrigatorias.add(calc2);
+        disciplinasObrigatorias.add(alg2);
+        disciplinasObrigatorias.add(alg2Pratica);
+        disciplinasObrigatorias.add(fis1);
+        disciplinasObrigatorias.add(intEst);
+        disciplinasObrigatorias.add(labFis1);
+        disciplinasObrigatorias.add(labTrans);
+        disciplinasObrigatorias.add(intEng);
         return disciplinasObrigatorias;
     }
 }
