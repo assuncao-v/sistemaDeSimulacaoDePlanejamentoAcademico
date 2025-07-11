4708cc4890f080479dbe4bbf5622b9a71a4d413d
Welder Victor <weldersalvadorvictor12@hotmail.com>
2025-07-11 17:47:25 -0300

    Implementação de novos métodos na classe PlanejamentoAcademico
    A classe planejamento academico agora carrega os dados das disciplinas e turmas. Além de adicionar um novo aluno ao planejamento.

--- planejamentoAcademico/src/main/java/br/edu/ufjf/dcc025/planejamentoacademico/PlanejamentoAcademico.java
+++ planejamentoAcademico/src/main/java/br/edu/ufjf/dcc025/planejamentoacademico/PlanejamentoAcademico.java
@@ -4,13 +4,32 @@
 
 package br.edu.ufjf.dcc025.planejamentoacademico;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  *
  * @author assuncao-v
  */
 public class PlanejamentoAcademico {
+    private Map<String,Disciplina> disciplinasCodigo = new LinkedHashMap<>();
+    private Map<String,Turma> turmasCodigo = new LinkedHashMap<>();
+    private List<Aluno> alunos = new ArrayList<>();
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    PlanejamentoAcademico(){
+        carregarDados();
     }
+
+    public void carregarDados(){
+        List<Disciplina> disciplinas = repositorioTurmasDisciplinas.carregarDisciplinas();
+        for(Disciplina d : disciplinas){
+            disciplinasCodigo.put(d.getCodigo(),d);
+        }
+    }
+    public void adicionAluno(Aluno aluno){
+        alunos.add(aluno);
+    }
+
 }
